8cfeafba053dc4fcb46061fe7efe4b1972619a9f
IshanDhruv <ishandhruv22@gmail.com>
2020-10-01 00:28:30 +0530

    Minor UI Changes

---
+++ lib/screens/info_page.dart
@@ -0,0 +1,55 @@
+//A simple About Us page. Changes here are not necessary.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class InfoPage extends StatefulWidget {
+  static String routename  = 'InfoPage';
+  InfoPage({Key key}) : super(key: key);
+
+  @override
+  _InfoPageState createState() => _InfoPageState();
+}
+
+class _InfoPageState extends State<InfoPage> {
+
+  String ieeeDesc = "IEEE VIT is one of the most active student chapters inside Region 10 of IEEE International. "
+      "We're a diverse group of tech enthusiasts, developers and designers.";
+
+  String hacktoberfestDesc = "Hacktoberfest is open to everyone in our global community. Whether you’re a developer, "
+      "student learning to code, event host, or company of any size, you can help drive growth of open source and make"
+      " positive contributions to an ever-growing community. ";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            GestureDetector(
+              child: Center(
+                child: Image.asset(
+                  "assets/images/ieee.png", //IEEE image
+                ),
+              ),
+              onTap: () => launch('https://ieeevit.org'), //Launches the IEEE website
+            ),
+            Text(ieeeDesc, style: TextStyle(color: Colors.white),),
+            GestureDetector(
+              child: Center(
+                  child: Container(
+                      child: SvgPicture.network('https://hacktoberfest.digitalocean.com/assets/HF-full-logo-b05d5eb32b3f3ecc9b2240526104cf4da3187b8b61963dd9042fdc2536e4a76c.svg'))),
+              onTap: () => launch('https://hacktoberfest.digitalocean.com'), //Launches Hacktoberfest site
+            ),
+            Text(hacktoberfestDesc, style: TextStyle(color: Colors.white)),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/contributor_card.dart
@@ -0,0 +1,46 @@
+//Widget used to show the contributor card. If you are changing this, make sure the updated design fits in with the design scheme of Hacktoberfest.
+
+import 'package:flutter/material.dart';
+
+class ContCard extends StatelessWidget {
+  final String userName;
+  final String dispImg;
+  final String desc;
+  final String webSite;
+  ContCard({this.desc,this.userName,this.dispImg,this.webSite});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 20),
+            Image.network(dispImg, height: 100),
+            SizedBox(height: 20),
+            Text('Username: $userName'),
+            SizedBox(height: 10),
+              Text('$desc'),
+            SizedBox(height: 10),
+              Text('WebSite: $webSite'),
+            SizedBox(height: 10),
+          ],
+        ),
+//        child: ListTile(
+//          title:Column(
+//            crossAxisAlignment: CrossAxisAlignment.start,
+//            children: <Widget>[
+//              Text('Username: $userName'),
+//              Text('$desc'),
+//              Text('WebSite: $webSite'),
+//            ],
+//          ),
+//          leading: Padding(
+//            padding: const EdgeInsets.all(8.0),
+//            child: Image.network(dispImg),
+//          ),
+//        ),
+      ),
+    );
+  }
+}
